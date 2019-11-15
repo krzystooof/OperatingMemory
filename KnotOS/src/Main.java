23ba2e5089bb56872dac65c60807b89cd409dfb2
Jcob <jchodubski@pm.me>
2019-11-15 14:49:42 +0100

    Adds Main class

---
+++ KnotOS/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        welcomeScreen();
+    }
+    /* WelcomeScreen method enables
+    * other sub-programs to
+    * run initialization methods
+    * and displays user-friendly
+    * loading screen
+     */
+    private static void welcomeScreen() {
+
+    }
+}
