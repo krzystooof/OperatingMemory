2a06c869cd8f6b642cc537a6cc8d2de54382783b
Jakub Chodubski <jakubchodubski@gmail.com>
2019-12-03 14:47:08 +0100

    Add Shell comments

--- KnotOS/src/Shell.java
+++ KnotOS/src/Shell.java
@@ -1,8 +1,22 @@
 public interface Shell {
-    public String[] getShellCommands();
-    public void pass (String[] params);
+    /* getShellCommands allows Interface class
+     * to read all commands from class implemeting
+     * Shell interface
+     * @return ArrayList<String> type field
+     * containing all commands accesible by user
+     */
+    public ArrayList<String> getShellCommands();
+    /* Is used to pass requests to
+     * class implementing Shell interface
+     * by Interface class
+     * @param params - ArrayList<String> type field
+     * containing full request collected from user
+     */
+    public void pass (ArrayList<String> params);
+    /* Public funtion displaing help
+     */
     public void getHelp();
-    private void help() {
-
-    }
+    /* Private function displaing help
+    */
+    private void help();
 }
