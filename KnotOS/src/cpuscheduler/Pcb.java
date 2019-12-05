e4de4e7b9ad6f47eaddce7fe454752a5daf73fea
aleksandergalganski <olekgalgan@gmail.com>
2019-12-05 13:47:11 +0100

    Pcb added

---
+++ KnotOS/src/cpuscheduler/Pcb.java
@@ -0,0 +1,44 @@
+public class Pcb {
+
+    int pid;
+    int priority;
+    ProcessState state;
+    String name;
+
+    public Pcb(int pid, int priority, ProcessState state, String name) {
+        this.pid = pid;
+        this.priority = priority;
+        this.state = state;
+        this.name = name;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public ProcessState getState() {
+        return state;
+    }
+
+    public void setState(ProcessState state) {
+        this.state = state;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Pcb{" +
+                "pid=" + pid +
+                ", priority=" + priority +
+                ", state=" + state +
+                ", name='" + name + '\'' +
+                '}' + "\n";
+    }
+}
